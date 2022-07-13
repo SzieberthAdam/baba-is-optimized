d3e402cb57ebe7e1a00e945c329b50d435144587
Szieberth Ádám <sziebadam@gmail.com>
2022-07-14 00:53:35 +0200

    autobaba-work loop

--- autobaba/autobaba-work.bat
+++ autobaba/autobaba-work.bat
@@ -1,3 +1,13 @@
 @echo off
 
+:loop
+
 python autobaba.py autobaba.txt autobaba-work-template.ahk
+
+set /p "answer=< Enter to repeat, Q to quit >"
+
+if /I [%answer%]==[q] goto end
+
+goto loop
+
+:end 
